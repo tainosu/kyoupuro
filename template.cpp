79c57f5d893b0ab8f6c81811464371b0dc65acb3
tainosu <sekikawa0916@gmail.com>
2024-03-09 18:15:56 +0900

    templateにrep macroを追加

--- template.cpp
+++ template.cpp
@@ -3,7 +3,13 @@ using namespace std;
 
 // alias
 using ll = long long;
+
 using vi = vector<int>;
+using vl = vector<ll>;
+
+// rep macro
+#define rep(var, a, b)  for(auto var = (a); var < (b); var++)
+#define rrep(var, a, b)  for(auto var = a - 1; var >= (b); var--)
 
 int main() {
 
