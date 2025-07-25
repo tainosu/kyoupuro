d42f216332178fa8d5680377de950f561be0f6ab
tainosu <sekikawa0916@gmail.com>
2025-07-25 15:46:01 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -7,6 +7,7 @@ using ll = long long;
 using vi = vector<int>;
 using vvi = vector<vi>;
 using vl = vector<ll>;
+using vs = vector<string>;
 
 // rep macro
 #define rep(var, a, b)  for(auto var = (a); var < (b); var++)
